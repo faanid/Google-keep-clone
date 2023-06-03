8b3453e648bec93d980eac77ee83892c6c644d67
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-06-03 23:46:05 +0430

    add headless ui and Switch toggle

--- google-keep-reactjs/src/components/Header.jsx
+++ google-keep-reactjs/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
 
 function Header(props) {
+  const [enabled, setEnabled] = useState(false);
   const logo = (
     <img
       src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
@@ -11,6 +14,20 @@ function Header(props) {
     <div className="header">
       {logo}
       <h3>Keep</h3>
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${
+          enabled ? "bg-blue-600" : "bg-gray-200"
+        } relative inline-flex h-6 w-11 items-center rounded-full`}
+      >
+        <span className="sr-only">Enable notifications</span>
+        <span
+          className={`${
+            enabled ? "translate-x-6" : "translate-x-1"
+          } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+        />
+      </Switch>
     </div>
   );
 }
